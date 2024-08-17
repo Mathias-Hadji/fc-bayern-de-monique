b67acf223bbef3ce292a90b496c3c135e490f914
Mathias Hadji <mathias.hadji@gmail.com>
2024-08-17 13:09:45 +0200

    ajout d'un footer et d'un logo au dessus du titre

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase/firebaseConfig";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import TeamTable from "./components/TeamTable";
+import logo from "./assets/logo.png";
+import Footer from "./components/Footer";
 
 function App() {
     const [players, setPlayers] = useState([]);
+    const [lastUpdated, setLastUpdated] = useState(null);
 
     useEffect(() => {
         async function loadPlayers() {
@@ -16,6 +19,13 @@ function App() {
                 ...doc.data(),
             }));
             setPlayers(playersData);
+
+            const latestUpdate = playersData.reduce((latest, player) => {
+                const playerUpdate = player.lastUpdated?.toDate(); // Convertir le timestamp Firestore en Date
+                return playerUpdate > latest ? playerUpdate : latest;
+            }, new Date(0)); 
+
+            setLastUpdated(latestUpdate);
         }
 
         loadPlayers();
@@ -23,9 +33,16 @@ function App() {
 
     return (
         <div className="App">
-            <h1>FC Bayern de Monique</h1>
-            <h2>Statistiques pour la saison 2024-2025</h2>
-            <TeamTable players={players} />
+            <div className="content">
+                <div className="title-container">
+                    <img src={logo} alt="Logo" className="logo" />
+                    <h1>FC Bayern de Monique</h1>
+                </div>
+                <h2>Statistiques pour la saison 2024-2025</h2>
+                <TeamTable players={players} />
+            </div>
+            <Footer lastUpdated={lastUpdated} />
+
             <SpeedInsights />
         </div>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import "../Footer.css";
+
+function Footer({ lastUpdated }) {
+    return (
+        <footer>
+            {lastUpdated && (
+                <p>
+                    Dernière mise à jour le {lastUpdated.toLocaleDateString()} à{" "}
+                    {lastUpdated.toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                    })}{" "}
+                    - Mathias Hadji
+                </p>
+            )}
+        </footer>
+    );
+}
+
+export default Footer;
